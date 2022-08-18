c2f2a3d8d1a3d22b91a686d88d79aecea72aa4d5
kylegav <60828879+kylegav@users.noreply.github.com>
2022-08-18 17:07:03 -0400

    Login action

---
+++ Watchpoint/LoginViewController.swift
@@ -0,0 +1,39 @@
+//
+//  LoginViewController.swift
+//  Watchpoint
+//
+//  Created by Kyle Gavin on 8/18/22.
+//  Copyright © 2022 Dan. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func loginButton(_ sender: Any) {
+        TwitterAPICaller.client?.login(url: "https://api.twitter.com/oauth/request_token", success: {
+            print("Login Sucessful")
+            self.performSegue(withIdentifier: "loginAction", sender: self)
+        
+        }, failure: { (Error) in
+            print("Login Failed")
+        })
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
