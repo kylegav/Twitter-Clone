875bf7eeeccf349ef8074f01689c09f59a66b2b6
kylegav <60828879+kylegav@users.noreply.github.com>
2022-09-11 15:37:34 -0400

    Added ComposeTweet View Controller and ComposeTweet view

---
+++ Watchpoint/ComposeTweetViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ComposeTweetViewController.swift
+//  Watchpoint
+//
+//  Created by Kyle Gavin on 9/11/22.
+//  Copyright © 2022 Dan. All rights reserved.
+//
+
+import UIKit
+
+class ComposeTweetViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
